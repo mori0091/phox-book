82466dcab256b7985bb6f005c8c348b16f52e82d
mori0091 <mori0091@users.noreply.github.com>
2026-07-26 02:24:23 +0000

    deploy: a67c1190dde33f498d06fd6431e4d3988a878cd8

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-31ecf0bb.js',
+                path_to_root + 'searchindex-872eb606.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
